f05aa1d02c9e97a090a18d0fb66438581ec1b869
Pavel Soloshenko <apodisation13@gmail.com>
2021-07-24 20:24:33 +0300

    L6 over: htmls added, view altered

--- women/views.py
+++ women/views.py
@@ -1,18 +1,29 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render
+from women.models import *
+
+
+MENU = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
 
 def home(request):
-    msg = '<h1><font color="green">Это главная страница...</font></h1>'
-    return HttpResponse(msg)
+    template_name = 'women/home.html'
+    women = Women.objects.all()
+    context = {'women': women}
+    return render(request, template_name, context)
 
 
 def categories(request, category_id):
-    print(request.GET)
-    if int(category_id) > 10:
-        raise Http404()
+    # print(request.GET)
+    # if int(category_id) > 10:
+    #     raise Http404()
 
-    msg = f'<h2><font color="blue">Страница категорий</font><br>{category_id}</h2>'
-    return HttpResponse(msg)
+    template_name = 'women/categories.html'
+    context = {
+        'category_id': category_id,
+        'menu': MENU
+    }
+    return render(request, template_name, context)
 
 
 def page_not_found(request, exception):
