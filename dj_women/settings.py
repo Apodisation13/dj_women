9c103bdd7eaa1e602912d3f532713fad78cf8bbb
james <Apodisation13@gmail.com>
2021-08-09 13:05:01 +0300

    L15 over. CLASS_BASED_VIEWS ADDED via views_CBV.py and urls.py altered to CBV

--- dj_women/settings.py
+++ dj_women/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6+g57lue(0_ot2*brc)wixl#^6g!psyx_ynn$vwjn48l*lult7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
